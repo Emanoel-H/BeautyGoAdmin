ea1ca883b6c05ca03169477a9c227df7ac79de4f
Emanoel <emanoel.pluto@gmail.com>
2026-07-03 18:35:34 -0300

    building IRepositorio interface

--- src/interfaces/IRepositorio.pas
+++ src/interfaces/IRepositorio.pas
@@ -1,7 +1,16 @@
 unit IRepositorio;
 
 interface
-
+uses
+  System.SysUtils, System.Generics.Collections;
+type
+  Repositorio<T: class> = interface
+    procedure Inserir(AEntidade: T);
+    procedure Atualizar(AEntidade: T);
+    procedure Deletar(AEntidade: T);
+    function BuscarPorId(AId: Int64): T;
+    function BuscarTodos: TObjectList<T>;
+  end;
 implementation
 
 end.
