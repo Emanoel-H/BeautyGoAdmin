27fa320b90494d888f8971a94119a1e6a36f0897
Emanoel <emanoel.pluto@gmail.com>
2026-07-03 14:18:07 -0300

    editing .gitignore

--- BeautyGoAdmin.dpr
+++ BeautyGoAdmin.dpr
@@ -3,13 +3,16 @@ program BeautyGoAdmin;
 uses
   Vcl.Forms,
   uFrmMain in 'src\forms\uFrmMain.pas' {frmMain},
-  uDmConexao in 'src\connection\uDmConexao.pas' {dmConexao: TDataModule};
+  uDmConexao in 'src\connection\uDmConexao.pas' {dmConexao: TDataModule},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows11 Dark');
   Application.CreateForm(TfrmMain, frmMain);
   Application.CreateForm(TdmConexao, dmConexao);
   Application.Run;
