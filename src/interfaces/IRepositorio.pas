af7d44996232f342125e2cff93a0c094e9f5d99a
Emanoel <emanoel.pluto@gmail.com>
2026-07-04 16:05:48 -0300

    removing BuscarTodos() adding listar() at IRepositorio

--- src/interfaces/IRepositorio.pas
+++ src/interfaces/IRepositorio.pas
@@ -2,14 +2,14 @@ unit IRepositorio;
 
 interface
 uses
-  System.SysUtils, System.Generics.Collections;
+  System.SysUtils, System.Generics.Collections, FireDAC.Comp.Client;
 type
   Repositorio<T: class> = interface
     procedure Inserir(AEntidade: T);
     procedure Atualizar(AEntidade: T);
     procedure Deletar(AEntidade: T);
     function BuscarPorId(AId: Int64): T;
-    function BuscarTodos: TObjectList<T>;
+    function Listar(AEntidade: T): TFDQuery;
   end;
 implementation
 
