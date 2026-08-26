979c0d942eeb4dde2dc0ebfc32433972102adea0
Emanoel <emanoel.pluto@gmail.com>
2026-08-26 11:02:20 -0300

    changing type of dPreco field to currency

--- src/model/uServicoOferecido.pas
+++ src/model/uServicoOferecido.pas
@@ -11,13 +11,13 @@ type
       iCategoriaId: Int64;
       iProfissionalId: Int64;
       sCodigo: string;
-      dPreco: Double;
+      dPreco: Currency;
     public
       property Id: Int64 read iId write iId;
       property CategoriaId: Int64 read iCategoriaId write iCategoriaId;
       property ProfissionalId: Int64 read iProfissionalId write iProfissionalId;
       property Codigo: string read sCodigo write sCodigo;
-      property dPreco: Double read dPreco write dPreco;
+      property dPreco: Currency read dPreco write dPreco;
   end;
 
 implementation
