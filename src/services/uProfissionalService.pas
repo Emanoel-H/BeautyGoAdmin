0c6e12fa317b208f11d7ce1ada19127d63cb23e2
Emanoel <emanoel.pluto@gmail.com>
2026-08-28 16:44:27 -0300

    creating methods signatures at uProfissionalService

--- src/services/uProfissionalService.pas
+++ src/services/uProfissionalService.pas
@@ -7,10 +7,65 @@ uses
 type
   TProfissionalService = class
     private
+      FDAO: Repositorio<TProfissional>;
+
+      constructor Create(AConexao: TFDConnection);
+      destructor Destroy; override;
+
+      procedure Salvar(AProfissional: TProfissional);
+      procedure Excluir(AProfissional: TProfissional);
+      function Listar(AProfissional: TProfissional): TFDQuery;
+
+      procedure Salvar(AServico: TServicoOferecido);
+      procedure Excluir(AServico: TServicoOferecido);
+      function Listar(AServico: TServicoOferecido): TFDQuery;
 
     public
 
   end;
 implementation
+
+{ TProfissionalService }
+
+constructor TProfissionalService.Create(AConexao: TFDConnection);
+begin
+
+end;
+
+destructor TProfissionalService.Destroy;
+begin
+
+  inherited;
+end;
+
+procedure TProfissionalService.Excluir(AProfissional: TProfissional);
+begin
+
+end;
+
+procedure TProfissionalService.Excluir(AServico: TServicoOferecido);
+begin
+
+end;
+
+function TProfissionalService.Listar(AServico: TServicoOferecido): TFDQuery;
+begin
+
+end;
+
+function TProfissionalService.Listar(AProfissional: TProfissional): TFDQuery;
+begin
+
+end;
+
+procedure TProfissionalService.Salvar(AProfissional: TProfissional);
+begin
+
+end;
+
+procedure TProfissionalService.Salvar(AServico: TServicoOferecido);
+begin
+
+end;
 
 end.
