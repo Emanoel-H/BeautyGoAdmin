750b87eada4e1e9a720f45fa5aeda2c0ae1567e2
Emanoel <emanoel.pluto@gmail.com>
2026-08-28 16:39:03 -0300

    building uProfissionalService initial structure

--- src/services/uProfissionalService.pas
+++ src/services/uProfissionalService.pas
@@ -4,7 +4,13 @@ interface
 uses
   System.SysUtils, IRepositorio, uProfissional, FireDAC.Comp.Client, uProfissionalDAO,
   Vcl.Forms, Winapi.Windows, uServicoOferecido;
+type
+  TProfissionalService = class
+    private
 
+    public
+
+  end;
 implementation
 
 end.
